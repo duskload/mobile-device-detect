28d23c12f86a2165271e92659c4f0b5582e1d79d
Michael Laktionov <duskload@gmail.com>
2018-06-14 23:20:40 +0300

    fixed typescript typings

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,8 @@
-declare module 'react-device-detect' {
-    import * as React from 'react';
-
+declare module 'mobile-device-detect' {
     export const isBrowser: boolean;
-
     export const isMobile: boolean;
-
     export const isTablet: boolean;
-
     export const isSmartTV: boolean;
-
     export const isConsole: boolean;
-
     export const isWearable: boolean;
 }
